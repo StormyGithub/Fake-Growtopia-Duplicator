bc12fb3e3075e87f0e14fb2047d243f4008650aa
StormyGithub <71922194+StormyGithub@users.noreply.github.com>
2020-12-12 18:42:52 +0100

    its a name :omg:

---
+++ StormDuplicator/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace StormDuplicator
+{
+    partial class StormDuplicator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StormDuplicator));
+            this.metroProgressBar1 = new MetroFramework.Controls.MetroProgressBar();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.metroButton4 = new MetroFramework.Controls.MetroButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.metroButton5 = new MetroFramework.Controls.MetroButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.metroButton6 = new MetroFramework.Controls.MetroButton();
+            this.metroProgressBar2 = new MetroFramework.Controls.MetroProgressBar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // metroProgressBar1
+            // 
+            this.metroProgressBar1.Location = new System.Drawing.Point(18, 69);
+            this.metroProgressBar1.Name = "metroProgressBar1";
+            this.metroProgressBar1.Size = new System.Drawing.Size(198, 23);
+            this.metroProgressBar1.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroProgressBar1.TabIndex = 2;
+            this.metroProgressBar1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroProgressBar1.Click += new System.EventHandler(this.metroProgressBar1_Click);
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Highlight = true;
+            this.metroButton1.Location = new System.Drawing.Point(170, 0);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(43, 28);
+            this.metroButton1.TabIndex = 4;
+            this.metroButton1.Text = "_";
+            this.metroButton1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Highlight = true;
+            this.metroButton2.Location = new System.Drawing.Point(211, 0);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(43, 28);
+            this.metroButton2.TabIndex = 5;
+            this.metroButton2.Text = "X";
+            this.metroButton2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.Highlight = true;
+            this.metroButton3.Location = new System.Drawing.Point(18, 28);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(198, 23);
+            this.metroButton3.TabIndex = 6;
+            this.metroButton3.Text = "Start Duplicating";
+            this.metroButton3.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton3.Click += new System.EventHandler(this.metroButton3_Click);
+            // 
+            // metroButton4
+            // 
+            this.metroButton4.Highlight = true;
+            this.metroButton4.Location = new System.Drawing.Point(12, 71);
+            this.metroButton4.Name = "metroButton4";
+            this.metroButton4.Size = new System.Drawing.Size(103, 23);
+            this.metroButton4.TabIndex = 7;
+            this.metroButton4.Text = "Inject Growtopia";
+            this.metroButton4.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton4.Click += new System.EventHandler(this.metroButton4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(-1, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 31);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "StormDuplicator";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // metroButton5
+            // 
+            this.metroButton5.Highlight = true;
+            this.metroButton5.Location = new System.Drawing.Point(18, 105);
+            this.metroButton5.Name = "metroButton5";
+            this.metroButton5.Size = new System.Drawing.Size(198, 23);
+            this.metroButton5.TabIndex = 12;
+            this.metroButton5.Text = "Restart";
+            this.metroButton5.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton5.Click += new System.EventHandler(this.metroButton5_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.metroButton6);
+            this.groupBox1.Controls.Add(this.metroProgressBar2);
+            this.groupBox1.Controls.Add(this.metroButton4);
+            this.groupBox1.Location = new System.Drawing.Point(6, 229);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(238, 107);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Injection";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(10, -6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 31);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Injection";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // metroButton6
+            // 
+            this.metroButton6.Highlight = true;
+            this.metroButton6.Location = new System.Drawing.Point(127, 71);
+            this.metroButton6.Name = "metroButton6";
+            this.metroButton6.Size = new System.Drawing.Size(103, 23);
+            this.metroButton6.TabIndex = 14;
+            this.metroButton6.Text = "Re-Inject";
+            this.metroButton6.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton6.Click += new System.EventHandler(this.metroButton6_Click);
+            // 
+            // metroProgressBar2
+            // 
+            this.metroProgressBar2.Location = new System.Drawing.Point(12, 39);
+            this.metroProgressBar2.Name = "metroProgressBar2";
+            this.metroProgressBar2.Size = new System.Drawing.Size(198, 23);
+            this.metroProgressBar2.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroProgressBar2.TabIndex = 3;
+            this.metroProgressBar2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.metroProgressBar1);
+            this.groupBox2.Controls.Add(this.metroButton5);
+            this.groupBox2.Controls.Add(this.metroButton3);
+            this.groupBox2.Location = new System.Drawing.Point(6, 64);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(239, 142);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick_2);
+            // 
+            // StormDuplicator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 341);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.metroButton2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "StormDuplicator";
+            this.Resizable = false;
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MetroFramework.Controls.MetroProgressBar metroProgressBar1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroButton metroButton3;
+        private MetroFramework.Controls.MetroButton metroButton4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private MetroFramework.Controls.MetroButton metroButton5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private MetroFramework.Controls.MetroProgressBar metroProgressBar2;
+        private System.Windows.Forms.Label label2;
+        private MetroFramework.Controls.MetroButton metroButton6;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ StormDuplicator/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StormDuplicator
+{
+    public partial class StormDuplicator : MetroFramework.Forms.MetroForm
+    {
+        public StormDuplicator()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BringToFront();
+            openanimation.AnimateWindow(this.Handle, 750, openanimation.CENTER);
+
+            timer1.Start();
+            timer1.Interval = 900; //control text speed flashig
+            timer2.Start();
+            timer2.Interval = 900; //control text speed flashig
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Loading...");
+            (metroProgressBar1).Value = (100);
+            MessageBox.Show("The Duplication Has Worked!");
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Application.ExitThread();
+            }
+            catch (Exception)
+            {
+                Environment.Exit(0);
+            }
+            return;
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+            return;
+        }
+
+        private void metroButton4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Loading...");
+            (metroProgressBar2).Value = (100);
+            MessageBox.Show("The Growtopia Has Been injected!");
+
+        }
+
+        private void metroProgressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+
+            int A = r.Next(0, 255);
+            int R = r.Next(0, 255);
+            int G = r.Next(0, 255);
+            int B = r.Next(0, 255);
+
+            label1.ForeColor = Color.FromArgb(A, R, G, B);
+        }
+
+        private void metroLabel1_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel1_Click_2(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void FontStyle(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButton5_Click(object sender, EventArgs e)
+        {
+            (metroProgressBar1).Value = (0);
+        }
+
+        private void metroButton6_Click(object sender, EventArgs e)
+        {
+            (metroProgressBar2).Value = (0);
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick_2(object sender, EventArgs e)
+        {
+            Random r = new Random();
+
+            int A = r.Next(0, 255);
+            int R = r.Next(0, 255);
+            int G = r.Next(0, 255);
+            int B = r.Next(0, 255);
+
+            label2.ForeColor = Color.FromArgb(A, R, G, B);
+        }
+    }
+}
